b27636d4079776be231f3bebd1746ad06f2afee4
Marco Oppel <52240770+moppel-dev@users.noreply.github.com>
2021-07-31 16:12:56 +0200

    Update age_in_secs.js

--- age_in_secs.js
+++ age_in_secs.js
@@ -1,21 +1,16 @@
-// Calculate age in seconds
-
 // calculate the user's age in seconds
 function calculate_age(age_year) {
     // get the months from the years
-    var months;
+    var months, days, hours, minutes;
     months = age_year * 12;
 
     // 30 days were used as a common denominator for each month
-    var days;
     days = months * 30;
     
     //get the hours from the days
-    var hours;
     hours = days * 24;
 
     //get the minutes from the hours
-    var minutes;
     minutes = hours * 60;
 
     //get the seconds from the hours
@@ -27,12 +22,12 @@ function calculate_age(age_year) {
 function get_age() {
     var age;
     try {
-        age = parseInt(prompt("What is your age in years: "));
+        age = parseInt( prompt("What is your age in years: ") );
     } catch (TypeError) {
-        alert("Invalid input entered");
+        console.log("Invalid input entered");
     }
     return age;
 };
 
 // output the user's age in seconds
-console.log("Age in seconds: " + calculate_age(get_age));
+console.log("Age in seconds: " + calculate_age(get_age()));
