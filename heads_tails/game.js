67da4eda34851afe06c0fed7408141a91bbc4a27
Marco Oppel <marOpp02@outlook.com>
2021-08-26 10:18:44 +0200

    Added Bootstrap

--- heads_tails/game.js
+++ heads_tails/game.js
@@ -1,3 +1,33 @@
+// heads img link "https://i.ibb.co/6J8mRcJ/image.png"
+// tails img link "https://i.ibb.co/fDyzd3s/image.png"
+
 document.addEventListener('DOMContentLoaded', () => {
-    //TODO
+    default_view();
 });
+
+function get_winner()
+{
+    //TODO
+};
+
+function display_winner()
+{
+    // remove the selection block and show the winner
+    const select = document.querySelector('#select-view');
+    const result = document.querySelector('#result-view');
+    const load = document.querySelector('#load-view');
+    select.style.display = 'none';
+    result.style.display = 'block';
+    load.style.display = 'none';
+}
+
+function default_view()
+{
+    // load the selection view by default
+    const select = document.querySelector('#select-view');
+    const result = document.querySelector('#result-view');
+    const load = document.querySelector('#load-view');
+    select.style.display = 'block';
+    result.style.display = 'none';
+    load.style.display = 'none';
+}
